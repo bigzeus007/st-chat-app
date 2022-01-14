557e16abb070f9eb1954e85e279e72fff76076ea
said.T <tarhisaid@gmail.com>
2022-01-14 01:06:24 +0100

    FIREBASE CONFIG CHANGED

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -7,17 +7,17 @@ import 'firebase/compat/auth';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDrsr63DkvNnQJD2iLqzG4yIUAA5TsdeZ8",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "st-chat-engine.firebaseapp.com",
+  databaseURL:
+    "https://st-chat-engine-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "st-chat-engine",
   storageBucket: "st-chat-engine.appspot.com",
   messagingSenderId: "806300775272",
   appId: "1:806300775272:web:651dde4c1b2e48859f5cdd",
-  measurementId: "G-J25E503SJH"
+  measurementId: "G-J25E503SJH",
 };
 
 // Initialize Firebase
 
-
-export const auth = firebase.initializeApp(firebaseConfig).auth()
-  
+export const auth = firebase.initializeApp(firebaseConfig).auth();
